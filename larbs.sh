07ca010c50f3debd8d6a183442bf52a54606f59a
James <james.ralph8555@gmail.com>
2022-08-04 01:33:56 -0400

    changed progs

--- larbs.sh
+++ larbs.sh
@@ -6,8 +6,8 @@
 
 ### OPTIONS AND VARIABLES ###
 
-dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/progs.csv"
+dotfilesrepo="https://github.com/james-ralph8555/voidrice.git"
+progsfile="https://raw.githubusercontent.com/james-ralph8555/LARBS/master/progs.csv"
 aurhelper="yay"
 repobranch="master"
 
